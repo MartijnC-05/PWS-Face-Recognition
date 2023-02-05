abd1bae9d44500248888ffad4ea00320b8465ca8
Martijn <74767447+MartijnC-05@users.noreply.github.com>
2023-02-05 14:39:34 +0100

    User interface iets aangepast en code opgeschoond

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ class App:
         self.main_window = tk.Tk()
         self.main_window.geometry("1200x520+350+100")                                                                                                           # Window maken
         
-        self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)                                                         # Login knop maken
+        self.login_button_main_window = util.get_button(self.main_window, 'Inloggen', 'green', self.login)                                                      # Login knop maken
         self.login_button_main_window.place(x=750, y=300)
         
-        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray', self.register_new_user, fg='black')          # Nieuw gezicht knop maken
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'Registreer gebruiker', 'gray', self.register_new_user, fg='black')       # Nieuw gezicht knop maken
         self.register_new_user_button_main_window.place(x=750, y=400)
         
         self.webcam_label = util.get_img_label(self.main_window)                                                                                                # Locatie voor webcam
@@ -55,10 +55,10 @@ class App:
         name = util.recognize(self.most_recent_capture_arr, self.db_dir)
         
         if name in ['unknown_person', 'no_persons_found']:
-            util.msg_box('Oh no...', 'Unknown user. Please register new user or try again.')                                                                    # Als de persoon niet herkend is krijgt hij een bericht
+            util.msg_box('Oh nee...', 'Onbekende gebruiker. Registreer een nieuwe gebruiker of probeer het opnieuw.')                                           # Als de persoon niet herkend is krijgt hij een bericht
             
         else:
-            util.msg_box('Welcome!', 'Welcome, {}.'.format(name))                                                                                               # Welkom bericht
+            util.msg_box('Welkom!', 'Welkom, {}.'.format(name))                                                                                                 # Welkom bericht
             with open(self.log_path, 'a') as f:                                                                                                                 # Houd een log bestand bij die bijhoudt wie en wanneer iemand is ingelogt
                 f.write('{},{}\n'.format(name, datetime.datetime.now()))
                 f.close()
@@ -67,10 +67,10 @@ class App:
         self.register_new_user_window = tk.Toplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")                                                                                              # Een nieuwe pagina wordt gemaakt voor het opslaan van een nieuw gezicht
         
-        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)          # Accepteer knop maken
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accepteren', 'green', self.accept_register_new_user)      # Accepteer knop maken
         self.accept_button_register_new_user_window.place(x=750, y=300)
     
-        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user)   # Try again knop maken
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Opnieuw proberen', 'red', self.try_again_register_new_user)   # Try again knop maken
         self.try_again_button_register_new_user_window.place(x=750, y=400)
     
         self.capture_label = util.get_img_label(self.register_new_user_window)                                                                                  # Foto die wordt gebruikt voor gezichtsherkenning wordt laten zien om te accepteren of opnieuw te proberen
@@ -81,7 +81,7 @@ class App:
         self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)                                                                  # Tekst bij tekstvak om te vertellen dat de gebruiker zijn naam daar moet invullen
         self.entry_text_register_new_user.place(x=750, y=150)
         
-        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, \ninput username:')                                     # Tekstvak voor de naam van de gebruiker
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Vul a.u.b, \nuw gebruikersnaam in:')                            # Tekstvak voor de naam van de gebruiker
         self.text_label_register_new_user.place(x=750, y=70)
         
     def try_again_register_new_user(self):                                                                                                                      # Wanneer op try again wordt geklikt ga je terug naar het hoofdmenu
@@ -102,23 +102,12 @@ class App:
         
         embeddings = face_recognition.face_encodings(self.register_new_user_capture)[0]
         file = open(os.path.join(self.db_dir, '{}.pickle'.format(name)), 'wb')
-        pickle.dump(embeddings, file)                                                               # Afbeelding wordt opgeslagen
+        pickle.dump(embeddings, file)                                                                                                                           # Afbeelding wordt opgeslagen
 
-        util.msg_box('Succes!','User was registered succesfully!')                                                                                              # Melding dat alles goed is gegaan
+        util.msg_box('Succes!','De gebruiker is succesvol geregistreerd!')                                                                                      # Melding dat alles goed is gegaan
         
         self.register_new_user_window.destroy()                                                                                                                 # Pagina om nieuwe gebruiker te registreren wordt gesloten
 
 if __name__ == "__main__":
     app = App()
     app.start()                                                                                                                                                 # App starten
-
-
-# WEG GONE LOES
-# unknown_img_path = './.tmp.jpg'
-        
-#         cv2.imwrite(unknown_img_path, self.most_recent_capture_arr)
-        
-#         output = str(subprocess.check_output(['face_recognition', self.db_dir, unknown_img_path]))                                                              # Kijken of de meest recente foto wordt herkend !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#         name = output.split(',')[1][:-3]
-#         os.remove(unknown_img_path)
-#         cv2.imwrite(os.path.join(self.db_dir, '{}.jpg'.format(name)), self.register_new_user_capture)
